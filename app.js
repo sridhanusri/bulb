b6c691bb8ba4aa394356e2e4e4dc952ce85bbcdf
sridhanusri <139197877+sridhanusri@users.noreply.github.com>
2023-07-12 14:30:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+       img.src="./image/on.png";
+}
+
+function bulb_off(){ 
+       img.src="./image/off.png";
+}
